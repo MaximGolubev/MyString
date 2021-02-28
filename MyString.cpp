1ce290f47dc5cc4fe410870b5cc1ebe9705c1eb4
delbfinych <dmi.villione@gmail.com>
2021-02-28 10:42:20 +0300

    Improve move assignment

--- MyString.cpp
+++ MyString.cpp
@@ -35,11 +35,12 @@ MyString& MyString::operator=(const MyString& other) {
 }
 
 MyString& MyString::operator=(MyString&& other) noexcept {
-    std::swap(_size, other._size);
-    std::swap(_data, other._data);
-    delete[] other._data;
-    other._data = nullptr;
-    other._size = 0;
+    if (this == &other) {
+        return *this;
+    }
+    delete[] _data;
+    _data = std::exchange(other._data, nullptr);
+    _size = std::exchange(other._size, 0);
     return *this;
 }
 
