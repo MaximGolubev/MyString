9bb31b2b2f9626ce038214686b432033527cf509
Максим Голубев <golubev@router.lab209.kaf>
2021-04-12 10:38:19 +0300

    Updated docs

--- docs/html/search/all_6d.js
+++ docs/html/search/all_6d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['mystring',['MyString',['../classMyString.html',1,'MyString'],['../classMyString.html#a68d7c86c6e92b8c142c8addcfb8e320f',1,'MyString::MyString(const char *rawString=nullptr)'],['../classMyString.html#a261187457de0d93e0041845f41b0bd12',1,'MyString::MyString(const MyString &amp;other)']]]
+  ['mystring',['MyString',['../classMyString.html',1,'MyString'],['../classMyString.html#a68d7c86c6e92b8c142c8addcfb8e320f',1,'MyString::MyString(const char *rawString=nullptr)'],['../classMyString.html#a261187457de0d93e0041845f41b0bd12',1,'MyString::MyString(const MyString &amp;other)']]],
+  ['mystring_2eh',['MyString.h',['../MyString_8h.html',1,'']]]
 ];
---
+++ docs/html/search/files_6d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mystring_2eh',['MyString.h',['../MyString_8h.html',1,'']]]
+];
